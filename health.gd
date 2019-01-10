eba36c924e009668dac76a376533752a8e94d448
Adam Hamed <44811380+Adam40471@users.noreply.github.com>
2019-01-10 15:50:19 -0500

    Version 0.0.8

--- health.gd
+++ health.gd
@@ -1,9 +1,12 @@
 #Adam Hamed
-#Initial Health Bar code
-#OCTOBER 9, 2017
+#Health Bar code
+#OCTOBER 10, 2017
 
 extends CanvasLayer
 
+var isAlive = true
+#Change this healthLevel value to test HP crystal mechanics functionality
+var healthLevel = 1
 var health = 100
 
 func _ready():
@@ -14,16 +17,31 @@ func _ready():
 #Controls how health decrements
 func _input(event):
 	#for testing
+	
 	if event.is_action_pressed("ui_accept"):
-		health -= 10
+		health = health - 10
 
 #Controls how health regenerates
 func _fixed_process(delta):
 	get_node("Control/ProgressBar").set_value(health)
 	
-	# Here is where we will do things when the health bar reaches 0
-	#if(health == 0):
-		#health = 100
+	#If there is no health left in the final level of health, you die
+	#currently need to make something where isAlive being false kills the player
+	if(healthLevel == 0):
+		isAlive = false
+	#This drops your health level down by 1 if you reach 0/100 health in your current lvl
+	#Need to make the crystal icons work with this, but the functionality is there.
+	elif(health == 0):
+		healthLevel = healthLevel - 1
+		#This is important. It refills the main health bar after dropping down a health level
+		#However, it does not refill it when you should be dead.
+		#Still need to make a HP crystal go away when you drop down a level
+		#Also need to make a function in the health pickup class to
+		#add increase your health level in the firstplace
+		if(healthLevel >= 1):
+			health = 100
+			
+		
 		
 	#health regeneration over time (for testing)
 	#health += delta * 2

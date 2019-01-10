f1e01e19b9c68960b49ec50ac47c44b7a9ad7d85
Adam Hamed <44811380+Adam40471@users.noreply.github.com>
2019-01-10 18:58:37 -0500

    Version 0.4.2

--- scripts/playerScripts/Bullets.gd
+++ scripts/playerScripts/Bullets.gd
@@ -3,12 +3,19 @@ extends Area2D
 var hit = false
 var direction = 0
 var velocity = Vector2(0,0)
+var timer = 0
+var sinAng = 0
 
 func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
+	timer += delta
+	sinAng = velocity.angle()
+	
 	self.set_pos(self.get_pos() + velocity)
+	#self.set_pos(self.get_pos() + velocity + Vector2((1/(timer+.2))*cos(timer*20),0).rotated(sinAng ))
+	
 
 func damageable(area):
 	if (area.get_name() != "aggroArea" && area.get_parent().has_method("decrease_Health")):
